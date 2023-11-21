530672c07ae1a45d504f35d3d5c98d0fa43785d7
Askarii <muhammadaskari267@gmail.com>
2023-11-21 22:36:43 +1100

    Add some extra things for use cases

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='m-4'>Please click the button below, we are under development. 
+      <br />
+      <Link href="/dashboard">
+        <button className='bg-white text-blue-400 py-4 px-4 rounded'>Dashboard</button>
+      </Link>
+    </div>
   )
 }
 
